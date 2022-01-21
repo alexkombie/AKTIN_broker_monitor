df3d3376a5abf446cf59579e7a185d6dc3803a04
Alexander Kombeiz <alexkombie@gmail.com>
2022-01-21 09:15:49 +0100

    fixed BrokerInfoFetcher, added abstract BrokerNodeFetcher, added FetcherManager ToDo BrokerErrorFetcher

--- src/node_to_csv.py
+++ src/node_to_csv.py
@@ -20,40 +20,18 @@
 #
 #
 
-import bisect
 import json
 import logging
 import os
 import sys
-from dataclasses import dataclass
-import traceback
 from abc import ABC, abstractmethod
-from bisect import bisect
+from dataclasses import dataclass
 from datetime import datetime
 from datetime import timedelta
 
 import lxml.etree as ET
-import chardet
 import pandas as pd
 import requests
-import sqlalchemy as db
-from sqlalchemy import exc
-
-
-class PropertiesJsonReader:
-    REQUIRED_KEYS = {'BROKER_URL', 'ADMIN_API_KEY', 'ROOT_DIR'}
-
-    def __init__(self, path):
-        if not os.path.isfile(path):
-            raise SystemExit('invalid config file path')
-        with open(path) as file_json:
-            dict_config = json.load(file_json)
-        set_found_keys = set(dict_config.keys())
-        set_matched_keys = self.REQUIRED_KEYS.intersection(set_found_keys)
-        if set_matched_keys != self.REQUIRED_KEYS:
-            raise SystemExit('following keys are missing in config file: {0}'.format(self.REQUIRED_KEYS.difference(set_matched_keys)))
-        for key in self.REQUIRED_KEYS:
-            os.environ[key] = dict_config.get(key)
 
 
 class CSVManager:
@@ -99,7 +77,7 @@ class BrokerNodeConnection(metaclass=SingletonMeta):
             url = '{}/{}'.format(url, item)
         return url
 
-    def get_broker_node_list(self) -> list[str]:
+    def get_broker_nodes_list(self) -> list[str]:
         url = self.__append_to_broker_url('broker', 'node')
         tree = self.__get_processed_response(url)
         return [node.find('id').text for node in tree.getchildren()]
@@ -119,15 +97,15 @@ class BrokerNodeConnection(metaclass=SingletonMeta):
         tree = self.__get_processed_response(url)
         stats = self.BrokerNodeStats(
                 tree.find('start').text,
-                tree.find('last-write').text,
-                tree.find('last-reject').text,
+                tree.find('last-write').text if tree.find('last-write') is not None else '',
+                tree.find('last-reject').text if tree.find('last-reject') is not None else '',
                 tree.find('imported').text,
                 tree.find('updated').text,
                 tree.find('invalid').text,
                 tree.find('failed').text)
         return stats
 
-    def get_broker_node_error(self, id_node: str):
+    def get_broker_node_errors(self, id_node: str):
         url = self.__append_to_broker_url('broker', 'node', id_node, 'stats')
         tree = self.__get_processed_response(url)
         errors = []
@@ -236,71 +214,50 @@ class BrokerNodeConnection(metaclass=SingletonMeta):
             return self.__CONTENT
 
 
-# ordner : 003
-# utf-8
-# csv file : 003_stats_2022.csv
-# error file : 003_errors_2022.csv
+class BrokerNodeFetcher(ABC):
+    _CSV_CATEGORY: str
+    _CSV_COLUMNS: list[str]
 
-# get information of broker nodes and saves them in a csv file
-# each node hsa a corresponding csv file
-# csv files are rotated each month
-# all csv files of a node are collected in a corresponding folder
-class BrokerNodeFetcherFactory:
+    def __init__(self, id_node: str, dir_working=''):
+        self._ID_NODE = id_node
+        self._DIR_WORKING = dir_working
+        self._CURRENT_DATE = datetime.now()
+        self._NAME_CSV = self._generate_csv_name(self._ID_NODE, self._CURRENT_DATE)
+        self._PATH_CSV = os.path.join(self._DIR_WORKING, self._NAME_CSV)
+        self._CSV_MANAGER = CSVManager(self._PATH_CSV)
+        self._BROKER_NODE_CONNECTION = BrokerNodeConnection()
 
-    # hjole alle ids
-    # create folder for each id
-    # create info und error fetcher for each folder
-    # fetcher braucht id und pfad zu csv
-    # factory erschafft csv-namen
-
-    def __init__(self):
-        #   self._NODE_ID = id_node.rjust(3, '0')
-        #   self._DIR_WORKING = self.__init_working_dir_for_fetcher()
-        self.__LIST_NODE_IDS = BrokerNodeConnection().get_broker_node_list()
-        self.__CURRENT_DATE = self.__get_current_date()
-
-    def create_info_fetcher(self, id_node: str):
+    def _generate_csv_name(self, id_node: str, date: datetime):
         id_node = id_node.rjust(3, '0')
-        dir_node = self.__init_working_dir_for_node(id_node)
-
-    @staticmethod
-    def __init_working_dir_for_node(id_node: str) -> str:
-        dir_root = os.environ['ROOT_DIR']
-        dir_working = os.path.join(dir_root, id_node)
-        if not os.path.isdir(dir_working):
-            os.makedirs(dir_working)
-        return dir_working
-
-
-class NodeInfoFetcher:
-
-    def __init__(self, dir_working: str, id_node: str):
-        self.__ID_NODE = id_node
-        self.__DIR_WORKING = dir_working
-        self.__CURRENT_DATE = datetime.now()
-        self.__NAME_CSV = self.__generate_csv_name(self.__ID_NODE, self.__CURRENT_DATE)
-        self.__PATH_CSV = os.path.join(self.__DIR_WORKING, self.__NAME_CSV)
-        self.__CSV_MANAGER = CSVManager(self.__PATH_CSV)
-        self.__BROKER_NODE_CONNECTION = BrokerNodeConnection()
-
-    @staticmethod
-    def __generate_csv_name(id_node: str, date: datetime):
-        id_node = id_node.rjust(3, '0')
-        name_csv = '_'.join([id_node, 'stats', str(date.year)])
+        name_csv = '_'.join([id_node, self._CSV_CATEGORY, str(date.year)])
         return ''.join([name_csv, '.csv'])
 
-    def init_new_csv(self):
-        if not os.path.isfile(self.__PATH_CSV):
-            df = pd.DataFrame(columns=['date', 'last_contact', 'start', 'last_write', 'last_reject',
-                                       'imported', 'updated', 'invalid', 'failed',
-                                       'daily_imported', 'daily_updated', 'daily_invalid', 'daily_failed', 'error_rate'])
-            self.__CSV_MANAGER.save_df_as_csv(df)
+    def init_working_csv(self):
+        if not os.path.isfile(self._PATH_CSV):
+            df = pd.DataFrame(columns=self._CSV_COLUMNS)
+            self._CSV_MANAGER.save_df_as_csv(df)
+
+    @staticmethod
+    def _convert_broker_time_to_local(date: str):
+        ts = pd.Timestamp(date).tz_convert('Europe/Berlin')
+        return ts.strftime("%Y-%m-%d %H:%M:%S")
+
+    @abstractmethod
+    def fetch_to_csv(self):
+        pass
+
+
+class NodeInfoFetcher(BrokerNodeFetcher):
+    _CSV_CATEGORY = 'stats'
+    _CSV_COLUMNS = ['date', 'last_contact', 'start', 'last_write', 'last_reject',
+                    'imported', 'updated', 'invalid', 'failed',
+                    'daily_imported', 'daily_updated', 'daily_invalid', 'daily_failed', 'error_rate']
 
     def fetch_to_csv(self):
         row_reference = pd.Series(dtype=str)
-        node = self.__BROKER_NODE_CONNECTION.get_broker_node(self.__ID_NODE)
-        stats = self.__BROKER_NODE_CONNECTION.get_broker_node_stats(self.__ID_NODE)
-        df = self.__CSV_MANAGER.read_csv_as_df()
+        node = self._BROKER_NODE_CONNECTION.get_broker_node(self._ID_NODE)
+        stats = self._BROKER_NODE_CONNECTION.get_broker_node_stats(self._ID_NODE)
+        df = self._CSV_MANAGER.read_csv_as_df()
         df = self.__delete_todays_row_if_exists(df)
         if df.empty:
             path_csv_last_year = self.__get_last_years_csv_path()
@@ -316,27 +273,27 @@ class NodeInfoFetcher:
             map_daily = self.__generate_empty_daily_stats()
         else:
             map_daily = self.__compute_daily_stats(row_reference, stats)
-        new_row = {'date':         self.__extract_year_month_day(self.__CURRENT_DATE),
-                   'last_contact': node.last_contact,
-                   'start':        stats.dwh_start,
-                   'last_write':   stats.last_write,
-                   'last_reject':  stats.last_reject,
+        new_row = {'date':         self.__extract_year_month_day(self._CURRENT_DATE),
+                   'last_contact': self._convert_broker_time_to_local(node.last_contact),
+                   'start':        self._convert_broker_time_to_local(stats.dwh_start),
+                   'last_write':   self._convert_broker_time_to_local(stats.last_write) if stats.last_write else '-',
+                   'last_reject':  self._convert_broker_time_to_local(stats.last_reject) if stats.last_reject else '-',
                    'imported':     stats.imported,
                    'updated':      stats.updated,
                    'invalid':      stats.invalid,
                    'failed':       stats.failed}
         new_row.update(map_daily)
         df = df.append(new_row, ignore_index=True)
-        self.__CSV_MANAGER.save_df_as_csv(df)
+        self._CSV_MANAGER.save_df_as_csv(df)
 
     def __delete_todays_row_if_exists(self, df: pd.DataFrame) -> pd.DataFrame:
         if not df.empty:
-            current_date = self.__extract_year_month_day(self.__CURRENT_DATE)
+            current_date = self.__extract_year_month_day(self._CURRENT_DATE)
             last_row = df.iloc[-1]
             if last_row.date == current_date:
                 df = df.head(-1)
             if any(df['date'] == current_date):
-                raise SystemExit('Date of today was found in past!!')
+                raise SystemExit('Date of today was found in multiple rows!!')
         return df
 
     @staticmethod
@@ -348,12 +305,12 @@ class NodeInfoFetcher:
         return '-'.join([str(date.year), month, day])
 
     def __get_last_years_csv_path(self):
-        date_last_year = self.__CURRENT_DATE.replace(year=self.__CURRENT_DATE.year - 1)
-        name_csv = self.__generate_csv_name(self.__ID_NODE, date_last_year)
-        return os.path.join(self.__DIR_WORKING, name_csv)
+        date_last_year = self._CURRENT_DATE.replace(year=self._CURRENT_DATE.year - 1)
+        name_csv = self._generate_csv_name(self._ID_NODE, date_last_year)
+        return os.path.join(self._DIR_WORKING, name_csv)
 
     def __check_end_of_year_transition(self, date_end_of_year: str) -> bool:
-        date_yesterday = self.__extract_year_month_day(self.__CURRENT_DATE - timedelta(days=1))
+        date_yesterday = self.__extract_year_month_day(self._CURRENT_DATE - timedelta(days=1))
         return date_end_of_year == date_yesterday
 
     @staticmethod
@@ -372,7 +329,7 @@ class NodeInfoFetcher:
         daily_failed = int(stats_current.failed) - int(stats_previous.failed)
         if (daily_imported + daily_updated) > 0:
             error_rate = (daily_failed + daily_invalid) / (daily_imported + daily_updated)
-            error_rate = round(error_rate, 2)
+            error_rate = round(error_rate * 100, 2)
         else:
             error_rate = '-'
         return {'daily_imported': daily_imported,
@@ -382,10 +339,56 @@ class NodeInfoFetcher:
                 'error_rate':     error_rate}
 
 
-# fetchers broker node errors
-# each clumn in csv file stands for one error
-class NodeErrorFetcher:
-    pass
+class NodeErrorFetcher(BrokerNodeFetcher):
+    _CSV_CATEGORY = 'errors'
+    _CSV_COLUMNS = ['timestamp', 'repeats', 'content']
+
+    def fetch_to_csv(self):
+        pass
+        """
+        errors = self.__BROKER_NODE_CONNECTION.get_broker_node_errors(self.__ID_NODE)
+        df = self.__CSV_MANAGER.read_csv_as_df()
+        for error in errors:
+            new_row = {
+                'timestamp': error.timestamp,
+                'repeats':   error.repeats if error.repeats is not None else '1',
+                'content':   error.content}
+            df = df.append(new_row, ignore_index=True)
+        self.__CSV_MANAGER.save_df_as_csv(df)
+        """
+
+
+class BrokerNodeFetcherManager:
+
+    def __init__(self):
+        self.__LIST_NODE_IDS = BrokerNodeConnection().get_broker_nodes_list()
+
+    def fetch_broker_node_information(self):
+        for id_node in self.__LIST_NODE_IDS:
+            name_folder = id_node.rjust(3, '0')
+            dir_working = self.__init_working_dir_for_node(name_folder)
+            self.__fetch_broker_node_stats(id_node, dir_working)
+            self.__fetch_broker_node_errors(id_node, dir_working)
+
+    @staticmethod
+    def __fetch_broker_node_stats(id_node: str, dir_working: str):
+        info = NodeInfoFetcher(id_node, dir_working)
+        info.init_working_csv()
+        info.fetch_to_csv()
+
+    @staticmethod
+    def __fetch_broker_node_errors(id_node: str, dir_working: str):
+        error = NodeErrorFetcher(id_node, dir_working)
+        error.init_working_csv()
+        error.fetch_to_csv()
+
+    @staticmethod
+    def __init_working_dir_for_node(name_folder: str) -> str:
+        dir_root = os.environ['ROOT_DIR']
+        dir_working = os.path.join(dir_root, name_folder)
+        if not os.path.isdir(dir_working):
+            os.makedirs(dir_working)
+        return dir_working
 
 
 def __init_logger():
@@ -399,14 +402,26 @@ def __stop_logger():
     logging.shutdown()
 
 
+def __load_properties_file_as_environment(path: str):
+    set_required_keys = {'BROKER_URL', 'ADMIN_API_KEY', 'ROOT_DIR'}
+    if not os.path.isfile(path):
+        raise SystemExit('invalid config file path')
+    with open(path) as file_json:
+        dict_config = json.load(file_json)
+    set_found_keys = set(dict_config.keys())
+    set_matched_keys = set_required_keys.intersection(set_found_keys)
+    if set_matched_keys != set_required_keys:
+        raise SystemExit('following keys are missing in config file: {0}'.format(set_required_keys.difference(set_matched_keys)))
+    for key in set_required_keys:
+        os.environ[key] = dict_config.get(key)
+
+
 def main(path_config: str):
     try:
         __init_logger()
-        PropertiesJsonReader(path_config)
-        a = NodeInfoFetcher('', '1')
-        a.init_new_csv()
-        a.add_column_to_csv()
-
+        __load_properties_file_as_environment(path_config)
+        fetcher = BrokerNodeFetcherManager()
+        fetcher.fetch_broker_node_information()
     except Exception as e:
         logging.exception(e)
     finally:
