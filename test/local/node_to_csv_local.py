237be89bb8ec339b2034100f53c72b4988cd09b6
Alexander Kombeiz <alexkombie@gmail.com>
2022-01-21 09:49:22 +0100

    smaller fix in local and architecture

--- test/local/node_to_csv_local.py
+++ test/local/node_to_csv_local.py
@@ -1,7 +1,11 @@
 import os
 import src.node_to_csv
+import time
 
 path_parent = os.path.dirname(os.getcwd())
 path_local = os.path.join(path_parent, 'local')
 path_settings = os.path.join(path_local, 'settings.json')
+
+start_time = time.time()
 src.node_to_csv.main(path_settings)
+print("--- %s seconds ---" % (time.time() - start_time))
