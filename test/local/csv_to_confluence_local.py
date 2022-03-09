564914b52c543009a1e325b135d261dd08749909
Alexander Kombeiz <alexkombie@gmail.com>
2022-03-09 09:39:06 +0100

    adjusted local execution scripts for split into script.py and common.py

--- test/local/csv_to_confluence_local.py
+++ test/local/csv_to_confluence_local.py
@@ -7,9 +7,9 @@ path_project = os.path.dirname(path_parent)
 path_src = os.path.join(path_project, 'src')
 sys.path.insert(0, path_src)
 
-import src.node_to_csv
+import src.csv_to_confluence
 
 path_test_settings = os.path.join(path_parent, 'local', 'settings.json')
 start_time = time.time()
-src.node_to_csv.main(path_test_settings)
+src.csv_to_confluence.main(path_test_settings)
 print("--- %s seconds ---" % (time.time() - start_time))
