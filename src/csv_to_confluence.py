ad80ceaa6875bf1e30dd56c445a58174b877c879
Alexander Kombeiz <alexkombie@gmail.com>
2022-03-22 10:01:32 +0100

    made TemplateResourceGetter not abstract anymore

--- src/csv_to_confluence.py
+++ src/csv_to_confluence.py
@@ -14,6 +14,7 @@ from common import __init_logger
 from common import __stop_logger
 from common import load_json_file_as_dict
 from common import load_properties_file_as_environment
+from common import SingletonMeta
 
 
 class CSVBackupManager:
@@ -34,34 +35,37 @@ class CSVBackupManager:
         return [name_file for name_file in os.listdir(self.__DIR_WORKING) if name_file.endswith('.csv')]
 
 
-class TemplateResourceHandler(ABC):
+class TemplateResourceGetter(metaclass=SingletonMeta):
 
-    def _get_resource_as_soup(self, path_resource: str):
+    def __init__(self):
+        self.__DIR_TEMPLATES = os.environ['CONFLUENCE_TEMPLATES_DIR']
+
+    def get_resource_as_string(self, name_resource: str):
+        path_resource = os.path.join(self.__DIR_TEMPLATES, name_resource)
         with open(path_resource, 'r') as resource:
             content = resource.read()
-        return self._convert_resource_to_soup(content)
+        return content
 
-    @staticmethod
-    def _convert_resource_to_soup(resource_template: str):
-        return bs4.BeautifulSoup(resource_template, 'html.parser')
+    def get_resource_as_soup(self, name_resource: str):
+        path_resource = os.path.join(self.__DIR_TEMPLATES, name_resource)
+        with open(path_resource, 'r') as resource:
+            content = resource.read()
+        return bs4.BeautifulSoup(content, 'html.parser')
 
 
-class TemplatePageHandler(TemplateResourceHandler, ABC):
+class TemplatePageContentWriter(ABC):
     _PAGE_TEMPLATE: bs4.BeautifulSoup
 
-    def __init__(self, id_node: str):
-        self.__ID_NODE = id_node
-
     def _add_content_to_template_page(self, page_template: str):
-        self._load_template_page_as_soup(page_template)
-        self._add_content_to_soup()
+        self._load_template_page_into_memory(page_template)
+        self._add_content_to_template_soup()
         return str(self._PAGE_TEMPLATE)
 
-    def _load_template_page_as_soup(self, page_template: str):
-        self._PAGE_TEMPLATE = self._convert_resource_to_soup(page_template)
+    def _load_template_page_into_memory(self, page_template: str):
+        self._PAGE_TEMPLATE = bs4.BeautifulSoup(page_template, 'html.parser')
 
     @abstractmethod
-    def _add_content_to_soup(self):
+    def _add_content_to_template_soup(self):
         pass
 
 
@@ -84,16 +88,15 @@ class NodeResourceFetcher:
         return self.__BROKER_NODE_CONNECTION.get_broker_node_resource(self.__ID_NODE, 'import-scripts')
 
 
-class TemplatePageNodeResourceWriter(TemplatePageHandler):
+class TemplatePageNodeResourceWriter(TemplatePageContentWriter):
 
     def __init__(self, id_node: str):
-        self.__ID_NODE = id_node
         self.__FETCHER = NodeResourceFetcher(id_node)
 
     def add_resources_to_template_page(self, page_template: str) -> str:
         return self._add_content_to_template_page(page_template)
 
-    def _add_content_to_soup(self):
+    def _add_content_to_template_soup(self):
         self.__add_versions_to_template_soup()
         self.__add_rscript_to_template_soup()
         self.__add_python_to_template_soup()
@@ -149,17 +152,15 @@ class CSVExtractor(CSVHandler):
         return df.to_dict('records')
 
 
-# TODO input just id??
-class TemplatePageCSVInfoWriter(TemplatePageHandler):
+class TemplatePageCSVInfoWriter(TemplatePageContentWriter):
 
-    def __init__(self, path_csv: str, id_node: str):
-        super().__init__(id_node)
+    def __init__(self, path_csv: str):
         self.__EXTRACTOR = CSVExtractor(path_csv)
 
     def add_node_stats_to_template_page(self, page_template: str) -> str:
         return self._add_content_to_template_page(page_template)
 
-    def _add_content_to_soup(self):
+    def _add_content_to_template_soup(self):
         self.__add_dates_to_template_soup()
         self.__add_global_imports_to_template_soup()
         self.__add_daily_imports_to_template_soup()
@@ -189,24 +190,23 @@ class TemplatePageCSVInfoWriter(TemplatePageHandler):
         self._PAGE_TEMPLATE.find(class_='error_rate_daily').string.replace_with(dict_row.get('daily_error_rate'))
 
 
-# TODO input just id??
-class TemplatePageCSVErrorWriter(TemplatePageHandler):
+class TemplatePageCSVErrorWriter(TemplatePageContentWriter):
     __FILENAME_TABLE_ERRORS: str = 'template_table_errors.html'
+    __NUM_ERRORS: int = 20
 
-    def __init__(self, path_csv: str, id_node: str):
-        super().__init__(id_node)
+    def __init__(self, path_csv: str):
         self.__EXTRACTOR = CSVExtractor(path_csv)
-        self.__NUM_ERRORS = 20
+        self.__RESOURCE_GETTER = TemplateResourceGetter()
 
     def add_node_errors_to_template_page(self, page_template: str) -> str:
         return self._add_content_to_template_page(page_template)
 
-    def _add_content_to_soup(self):
+    def _add_content_to_template_soup(self):
         table_errors = self.__create_confluence_error_table()
         self._PAGE_TEMPLATE.find(class_='table_errors_body').replace_with(table_errors)
 
     def __create_confluence_error_table(self) -> bs4.BeautifulSoup:
-        table_errors = self.__load_error_table_template_as_soup()
+        table_errors = self.__RESOURCE_GETTER.get_resource_as_soup(self.__FILENAME_TABLE_ERRORS)
         list_dicts_error = self.__EXTRACTOR.get_first_rows_as_list_of_dicts(self.__NUM_ERRORS)
         list_error_rows = []
         for dict_error in list_dicts_error:
@@ -215,33 +215,29 @@ class TemplatePageCSVErrorWriter(TemplatePageHandler):
         table_errors.find('tbody').extend(list_error_rows)
         return table_errors
 
-    def __load_error_table_template_as_soup(self) -> bs4.BeautifulSoup:
-        path_error_table = os.path.join(os.environ['CONFLUENCE_TEMPLATES_DIR'], self.__FILENAME_TABLE_ERRORS)
-        return self._get_resource_as_soup(path_error_table)
-
     def __create_error_table_row(self, timestamp: str, repeats: str, content: str) -> Tag:
-        column_timestamp = self.__create_table_row_column(timestamp, {'style': 'text-align: center;'})
-        column_repeats = self.__create_table_row_column(repeats, {'style': 'text-align: center;'})
-        column_content = self.__create_table_row_column(content)
+        column_timestamp = self.__create_table_row_data(timestamp, {'style': 'text-align: center;'})
+        column_repeats = self.__create_table_row_data(repeats, {'style': 'text-align: center;'})
+        column_content = self.__create_table_row_data(content)
         row_error = bs4.BeautifulSoup().new_tag('tr')
         row_error.extend([column_timestamp, column_repeats, column_content])
         return row_error
 
     @staticmethod
-    def __create_table_row_column(content: str, attributes=None) -> Tag:
+    def __create_table_row_data(content: str, attributes=None) -> Tag:
         attributes = {} if attributes is None else attributes
         row_column = bs4.BeautifulSoup().new_tag('td', attrs=attributes)
         row_column.append(content)
         return row_column
 
 
-class TemplatePageStatusChecker(TemplatePageHandler):
+class TemplatePageStatusChecker(TemplatePageContentWriter):
     __FILENAME_STATUS: str = 'template_element_status.html'
     __WITDH_IMPORT_THRESHOLD: float = 0.25
 
     def __init__(self, id_node: str):
-        super().__init__(id_node)
         self.__DAILY_IMPORT_TRESHOLD = self.__get_daily_import_threshold_from_mapping_table(id_node)
+        self.__RESOURCE_GETTER = TemplateResourceGetter()
 
     @staticmethod
     def __get_daily_import_threshold_from_mapping_table(id_node: str) -> str | None:
@@ -254,7 +250,7 @@ class TemplatePageStatusChecker(TemplatePageHandler):
     def check_and_set_status_of_template_page(self, page_template: str) -> str:
         return self._add_content_to_template_page(page_template)
 
-    def _add_content_to_soup(self):
+    def _add_content_to_template_soup(self):
         if self.__is_template_soup_offline():
             status = self.__create_status_element('OFFLINE', 'Red')
         elif self.__is_template_soup_not_importing():
@@ -275,6 +271,15 @@ class TemplatePageStatusChecker(TemplatePageHandler):
         last_write = self._PAGE_TEMPLATE.find(class_='last_write').string
         return self.__is_date_longer_ago_than_yesterday(last_write)
 
+    @staticmethod
+    def __is_date_longer_ago_than_yesterday(date: str) -> bool:
+        date_input = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+        date_now = datetime.now()
+        if date_now - date_input > timedelta(days=1):
+            return True
+        else:
+            return False
+
     def __are_template_soup_imports_deviating(self) -> bool:
         if self.__DAILY_IMPORT_TRESHOLD is not None:
             threshold = int(self.__DAILY_IMPORT_TRESHOLD)
@@ -292,25 +297,12 @@ class TemplatePageStatusChecker(TemplatePageHandler):
         error_rate = float(self._PAGE_TEMPLATE.find(class_='error_rate_daily'))
         return error_rate >= 1.0
 
-    @staticmethod
-    def __is_date_longer_ago_than_yesterday(date: str) -> bool:
-        date_input = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-        date_now = datetime.now()
-        if date_now - date_input > timedelta(days=1):
-            return True
-        else:
-            return False
-
     def __create_status_element(self, title: str, color: str) -> bs4.BeautifulSoup:
-        status = self.__load_status_template_as_soup()
+        status = self.__RESOURCE_GETTER.get_resource_as_soup(self.__FILENAME_STATUS)
         status_title = self.__create_status_element_parameter('title', title)
         status_color = self.__create_status_element_parameter('colour', color)
         status.find('ac:structured-macro').extend([status_title, status_color])
         return status
-
-    def __load_status_template_as_soup(self) -> bs4.BeautifulSoup:
-        path_status = os.path.join(os.environ['CONFLUENCE_TEMPLATES_DIR'], self.__FILENAME_STATUS)
-        return self._get_resource_as_soup(path_status)
 
     @staticmethod
     def __create_status_element_parameter(p_type: str, p_content: str) -> Tag:
@@ -319,24 +311,24 @@ class TemplatePageStatusChecker(TemplatePageHandler):
         return parameter
 
 
-class TemplatePageJiraTableWriter(TemplatePageHandler):
+class TemplatePageJiraTableWriter(TemplatePageContentWriter):
     __FILENAME_TABLE_JIRA: str = 'template_table_jira.html'
 
     def __init__(self, id_node: str):
-        super().__init__(id_node)
         global dict_mapping
         self.__LABELS_JIRA = dict_mapping[id_node]['JIRA_LABELS']
+        self.__RESOURCE_GETTER = TemplateResourceGetter()
 
     def add_jira_table_to_template_page(self, page_template: str) -> str:
         return self._add_content_to_template_page(page_template)
 
-    def _add_content_to_soup(self):
+    def _add_content_to_template_soup(self):
         table_jira = self.__generate_jira_table_for_confluence()
         self._PAGE_TEMPLATE.find(class_='table_jira').replace_with(table_jira)
 
     def __generate_jira_table_for_confluence(self) -> bs4.BeautifulSoup:
         query = self.__generate_jira_query_from_labels()
-        table = self.__load_jira_table_as_soup()
+        table = self.__RESOURCE_GETTER.get_resource_as_soup(self.__FILENAME_TABLE_JIRA)
         table = str(table).replace('[MY_JQL_QUERY]', query)
         table = bs4.BeautifulSoup(table, 'html.parser')
         return table
@@ -348,180 +340,24 @@ class TemplatePageJiraTableWriter(TemplatePageHandler):
         query = ' OR '.join(tmp_labels)
         return query
 
-    def __load_jira_table_as_soup(self) -> bs4.BeautifulSoup:
-        path_status = os.path.join(os.environ['CONFLUENCE_TEMPLATES_DIR'], self.__FILENAME_TABLE_JIRA)
-        return self._get_resource_as_soup(path_status)
 
-class ConfluencePageHandler:
-
-    def __init__(self, id_node: str, dir_working=''):
-        self.__ID_NODE = id_node
-        self.__DIR_WORKING = dir_working
-        global dict_mapping
-        self.__COMMON_NAME = dict_mapping[id_node]['COMMON']
-        self.__JIRA_LABELS = dict_mapping[id_node]['JIRA-LABELS']
-        self.__CONFLUENCE = ConfluenceConnection()
-
-
-class ConfluenceHandlerManager:
-
-    def __init__(self):
-        self.__CONFLUENCE = ConfluenceConnection()
-        self.__CONFLUENCE_ROOT_PAGE_NAME = 'Support'
-        self.__CONFLUENCE_PARENT_PAGE_NAME = 'Support Log Broker-Monitor'
-        self.__init_parent_page()
-
-    def __init_parent_page(self):
-        if not self.__CONFLUENCE.check_page_existence(self.__CONFLUENCE_PARENT_PAGE_NAME):
-            self.__CONFLUENCE.create_confluence_page(self.__CONFLUENCE_PARENT_PAGE_NAME, self.__CONFLUENCE_ROOT_PAGE_NAME)
-
-
-"""
-
-# INIT PARENT
-# CREATE NEW PAGES BY MAPPING TABLE (with jira table)
-# UPDATE PAGES
-# MERGE PAGES
-
-
-"""
-
-# upload_attachement()
-
-# update element
-# html.find(class_='version_template').string.replace_with('Version 2.0')
-
-# update jira
-# html.find(class_='table_jira').replace_with('abcedf')
-
-# html = bs(content, 'html.parser')
-# _ = html.find(id='it-contact').string.replace_with('12345')
-
-# broker connection for version/rscript/python/import-scripts
-
-
-"""
-class ConfluencePageHandler:
-
-    def __init__(self, id_node: str, dir_working=''):
-        self.__ID_NODE = id_node
-        self.__DIR_WORKING = dir_working
-        self.__EXTRACTOR_STATS = self.__generate_csv_extractor(id_node, 'stats')
-        self.__EXTRACTOR_ERRORS = self.__generate_csv_extractor(id_node, 'errors')
-        self.__RESOURCE_FETCHER = NodeResourceFetcher(id_node)
-        self.__TEMPLATE_CONFLUENCE = self.__load_project_resource_as_html_soup('template_page.html')
-        global dict_mapping
-        self.__COMMON_NAME = dict_mapping[id_node]['CN']
-        self.__JIRA_LABELS = dict_mapping[id_node]['JIRA']
-        self.__CONFLUENCE = ConfluenceConnection()
-        self.upload_node_information_as_confluence_page()
-
-    def __generate_csv_extractor(self, id_node: str, csv_category: str) -> CSVExtractor:
-        """
-        Builds the name of the csv according to the specifications of node_to_csv.py
-        """
-        id_node_spread = id_node.rjust(3, '0')
-        current_year = str(datetime.now().year)
-        name_csv = ''.join([id_node_spread, '_', csv_category, '_', current_year, '.csv'])
-        path_csv = os.path.join(self.__DIR_WORKING, name_csv)
-        return CSVExtractor(path_csv)
-
-    @staticmethod
-    def __load_project_resource_as_html_soup(name_resource: str) -> bs4.BeautifulSoup:
-        dir_resources = os.environ['RESOURCES_DIR']
-        path_resource = os.path.join(dir_resources, name_resource)
-        with open(path_resource, 'r') as file:
-            resource = file.read()
-        soup = bs4.BeautifulSoup(resource, 'html.parser')
-        return soup
-
-    def upload_node_information_as_confluence_page(self):
-        if not self.__CONFLUENCE.check_page_existence(self.__COMMON_NAME):
-            print('create new page dummy')
-            # set jira labels
-        else:
-            print('get page content')
-        content = self.__CONFLUENCE.get_page_content('Dummy Broker-Monitor')
-        print(type(content))
-        # get page version
-        # if newer version available
-        # migrate static information
-
-        # html = bs(test, 'html.parser')
-
-        # fetch broker resources to page
-        # fetch csv stats to page
-        # fetch csv errors to page
-        # create or update page
-
-    # TODO set status
-
-    def __put_jira_table_to_confluence_page(self, page_confluence: bs4.BeautifulSoup) -> bs4.BeautifulSoup:
-        table_jira = self.__generate_jira_table_for_confluence()
-        page_confluence.find(class_='table_jira').replace_with(table_jira)
-        return page_confluence
-
-    def __generate_jira_table_for_confluence(self) -> bs4.BeautifulSoup:
-        query = self.__generate_jira_query()
-        table = self.__load_project_resource_as_html_soup('table_jira.html')
-        table = str(table).replace('[MY_JQL_QUERY]', query)
-        table = bs4.BeautifulSoup(table, 'html.parser')
-        return table
-
-    def __generate_jira_query(self) -> str:
-        tmp_labels = self.__JIRA_LABELS
-        for idx, label in enumerate(tmp_labels):
-            tmp_labels[idx] = ''.join(['Labels=\"', label, '\"'])
-        query = ' OR '.join(tmp_labels)
-        return query
-
-
-# html.replace('[MY_JQL_QUERY]', '\"ABCD\"')
-# jira_query = 'Labels="klinikum-fuerth" OR Labels="fuerth" OR Labels="fürth"'
-# [MY_JQL_QUERY]
-# 'Labels="klinikum-fuerth" OR Labels="fuerth" OR Labels="fürth"'
-# table_jira = ''
-# print(table_jira)
-#
-# return html
-
-"""
-class ConfluenceHandlerManager:
-
-    def __init__(self):
-        self.DIR_ROOT = os.environ['ROOT_DIR']
-        global dict_mapping
-        self.DICT_MAPPING = dict_mapping
-
-    def upload_broker_node_csv_to_confluence(self):
-        for id_node in self.LIST_NODE_IDS:
-            self.__fetch_broker_node_stats(id_node, dir_working)
-            self.__fetch_broker_node_errors(id_node, dir_working)
-
-    def get_working_directory(self, id_node: str) -> str:
-        name_folder = id_node.rjust(3, '0')
-        dir_working = os.path.join(self.DIR_ROOT, name_folder)
-        return dir_working
-"""
-
-
-def main(path_config: str, path_mapping: str):
+def main(path_config: str):
     try:
         __init_logger()
         load_properties_file_as_environment(path_config)
         global dict_mapping
-        dict_mapping = load_mapping_table_as_dict(path_mapping)
+        dict_mapping = load_json_file_as_dict(os.environ['CONFLUENCE_MAPPING_JSON'])
+
+        path_html = os.path.join(os.environ['CONFLUENCE_TEMPLATES_DIR'], 'template_page.html')
+        with open(path_html, 'r') as file:
+            html2 = file.read()
+
+        c1 = TemplatePageStatusChecker('2')
+        c1.check_and_set_status_of_template_page(html2)
+        a = c1.create_status_element('ONLINE', 'Green')
+
+        print(a)
     except Exception as e:
         logging.exception(e)
     finally:
         __stop_logger()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        raise SystemExit('path to config file is missing')
-    if len(sys.argv) == 2:
-        raise SystemExit('path to mapping file is missing')
-    if len(sys.argv) > 3:
-        raise SystemExit('invalid number of input arguments')
-    main(sys.argv[1], sys.argv[2])
