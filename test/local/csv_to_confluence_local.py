bef25d299899f8572bf348c4732b47836c74a56b
Alexander Kombeiz <alexkombie@gmail.com>
2022-11-10 14:48:23 +0100

    moved test configuration into resources-folder, adjusted local tests

--- test/local/csv_to_confluence_local.py
+++ test/local/csv_to_confluence_local.py
@@ -4,12 +4,12 @@ import time
 from pathlib import Path
 
 this_path = Path(os.path.realpath(__file__))
-path_src = os.path.join(this_path.parents[3], 'src')
+path_src = os.path.join(this_path.parents[2], 'src')
 sys.path.insert(0, path_src)
 
 import csv_to_confluence
 
-path_settings = os.path.join(this_path.parents[1], 'settings.json')
+path_settings = os.path.join(this_path.parents[1], 'resources', 'settings.json')
 start_time = time.time()
 csv_to_confluence.main(path_settings)
 print("--- %s seconds ---" % (time.time() - start_time))
